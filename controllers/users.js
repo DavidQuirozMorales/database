4da9e2ccc9e68a2b2cc2775ebab734dedbad9718
david quiroz morales <davidquirozmorales@hotmail.com>
2023-10-10 13:20:50 -0600

    Consulta formal a la BD

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,26 @@
-const {request, response} = require('express');
+const { request, response } = require('express');
+const usersModel = require('../models/users')
+const pool = require('../db');
 
+const listUsers = async (req = request, res = response) => {
+    let conn;
 
-const listUsers = (req = request,res = response) => {
-    res.json({msg: "Hola usuario"})
+    try {
+        conn = await pool.getConnection();
 
+        const users = await conn.query(usersModel.getAll, (err) =>{
+            if(err){
+                throw err
+            }
+        });
+        
+        res.json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }finally{
+        if (conn) conn.end();
+    }
 }
 
-module.exports = {listUsers};
+module.exports = { listUsers };
