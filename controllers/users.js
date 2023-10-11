bfa3c9609d5e71b5b61978153eaa23f13b967e48
david quiroz morales <davidquirozmorales@hotmail.com>
2023-10-11 13:10:04 -0600

    Manejo de ENV y nuevo Endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -23,4 +23,36 @@ const listUsers = async (req = request, res = response) => {
     }
 }
 
-module.exports = { listUsers };
+const listUserByID = async (req = request, res = response) => {
+    const {id} = req.params;
+
+    if (isNaN(id)){
+        res.status(404).json({msg: 'Invalid ID'});
+        return;
+    }
+
+    let conn;
+
+    try {
+        conn = await pool.getConnection();
+
+        const [user] = await conn.query(usersModel.getByID, [id], (err) =>{
+            if(err){
+                throw err
+            }
+        });
+
+        if (!user){
+            res.status(404).json({msg:'User not found'});
+            return;
+        }
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }finally{
+        if (conn) conn.end();
+    }
+}
+
+module.exports = { listUsers, listUserByID };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 const {Router} = require('express');
-const {listUsers} = require('../controllers/users')
+const {listUsers, listUserByID} = require ('../controllers/users')
+
 const router = Router();
 
-//http://localhost:3000/api/v1/users/???
-router.get('/', listUsers);
-
+//http://localhost:3000/api/v1/users/??
+router.get('/',listUsers);
+//se coloca dos puntos porque id es un valor variable
+router.get('/:id',listUserByID);
 //router.post('/',listUsers);
 //router.put('/',listUsers);
 //router.patch('/',listUsers);
-//router.delete('/', listUsers);
-module.exports = router;
+//router.delete('/',listUsers);
+
+module.exports= router;
